8548a3eca5271178f8bcd72478701a6299bc59c8
Cristian <179995201+CrisLmon@users.noreply.github.com>
2025-05-27 20:40:19 -0500

    feat: implementacion de clase Transmision

--- src/App.java
+++ src/App.java
@@ -1,12 +1,18 @@
 import models.Motor;
+import models.Transmision;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Motor motor = new Motor("M001", "Bosch", 4, 120);
+        Transmision transmision = new Transmision("T001", "ZF", "Automática", 6);
 
         System.out.println("==== MOTOR ====");
         motor.mostrarInformacion();
         motor.encenderMotor();
+
+        System.out.println("\n==== TRANSMISIÓN ====");
+        transmision.mostrarInformacion();
+        transmision.cambiarMarcha(3);
 
     }
 }
